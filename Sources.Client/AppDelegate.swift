faf286a07c23ce7dd9434a16c482d0d9d1627fe8
Max Howell <mxcl@me.com>
2018-07-10 12:26:41 -0400

    Client skeleton

---
+++ Sources.Client/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GitBell
+//
+//  Created by Max Howell on 7/10/18.
+//  Copyright © 2018 Max Howell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
