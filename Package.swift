5c69e33313fc4d7ad01fcfbef7d80b85b908dfcb
Max Howell <mxcl@me.com>
2018-07-10 11:17:21 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "gitbell",
+    products: [
+        .executable(name: "gitbell", targets: ["gitbell"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.10")
+    ],
+    targets: [
+        .target(name: "gitbell", dependencies: ["Socket"], path: "Sources")
+    ]
+)
