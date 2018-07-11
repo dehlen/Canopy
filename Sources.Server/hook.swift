9783965a4a2cf58dec50b48913ac03e0df42a2e3
Max Howell <mxcl@me.com>
2018-07-11 14:41:59 -0400

    Add a whole bunch of hook translations

--- Sources.Server/hook.swift
+++ Sources.Server/hook.swift
@@ -4,18 +4,20 @@ import PerfectHTTP
 protocol Notificatable {
     var title: String? { get }
     var body: String? { get }
+    var url: URL? { get }
 }
 
 extension Notificatable {
     var body: String? { return nil }
+    var url: URL? { return nil }
 }
 
 struct PingEvent: Decodable, Notificatable {
     let hook: Hook
-    let sender: Sender
+    let sender: User
 
     // which is set depends on Hook.type
-    let organization: Sender?
+    let organization: User?
     let repository: Repository?
 
     struct Hook: Decodable {
@@ -36,10 +38,468 @@ struct PingEvent: Decodable, Notificatable {
     }
 }
 
-struct Sender: Decodable {
+struct CheckRunEvent: Decodable, Notificatable {
+    let action: String
+    let check_run: CheckRun
+    let repository: Repository
+
+    struct CheckRun: Decodable {
+        let url: URL
+        let status: String
+    }
+
+    var title: String? {
+        return "Check run \(check_run.status)"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return check_run.url
+    }
+}
+
+struct CheckSuiteEvent: Decodable, Notificatable {
+    let action: String
+    let check_suite: CheckSuite
+    let repository: Repository
+
+    struct CheckSuite: Decodable {
+        let url: URL
+        let status: String
+    }
+
+    var title: String? {
+        return "Check suite \(check_suite.status)"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return check_suite.url
+    }
+}
+
+struct CommitComment: Decodable, Notificatable {
+    let action: String
+    let comment: Comment
+    let repository: Repository
+
+    var title: String? {
+        return "\(comment.user.login) commented on commit"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return comment.url
+    }
+}
+
+struct CreateEvent: Decodable, Notificatable {
+    let repository: Repository
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) created a repository"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return repository.html_url
+    }
+}
+
+struct DeleteEvent: Decodable, Notificatable {
+    let repository: Repository
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) deleted a repository"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return repository.html_url  // but… will 404
+    }
+}
+
+struct DeploymentEvent: Decodable, Notificatable {
+    let repository: Repository
+    let deployment: Deployment
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) deployed to \(deployment.environment)"
+    }
+    var body: String? {
+        return [deployment.description, repository.full_name].compactMap{ $0 }.joined(separator: ", ")
+    }
+    var url: URL? {
+        return deployment.url
+    }
+}
+
+struct DeploymentStatusEvent: Decodable, Notificatable {
+    let deployment_status: DeploymentStatus
+    let deployment: Deployment
+    let repository: Repository
+    let sender: User
+
+    struct DeploymentStatus: Decodable {
+        let url: URL
+        let status: String
+        let description: String?
+    }
+
+    var title: String? {
+        return "\(sender.login) deployed to \(deployment.environment)"
+    }
+    var body: String? {
+        return deployment_status.status
+    }
+    var url: URL? {
+        return deployment_status.url
+    }
+}
+
+struct ForkEvent: Decodable, Notificatable {
+    let forkee: Repository
+    let repository: Repository
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) forked \(forkee.full_name)"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return repository.html_url
+    }
+}
+
+struct GollumEvent: Decodable, Notificatable {
+    let pages: [Page]
+    let repository: Repository
+    let sender: User
+
+    struct Page: Decodable {
+        let page_name: String
+        let title: String
+        let summary: String?
+        let action: String
+        let html_url: URL
+    }
+
+    var title: String? {
+        return "\(sender.login) wiki’d"
+    }
+    var body: String? {
+        return "\(pages.count) wiki events"
+    }
+    var url: URL? {
+        return pages.first?.html_url
+    }
+}
+
+struct InstallationEvent: Decodable, Notificatable {
+    let action: String
+    let installation: Installation
+    let repositories: [Repository]
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) \(action) a GitHub app \(installation.app_id)"
+    }
+    var body: String? {
+        return "\(repositories.count) repos affected"
+    }
+    var url: URL? {
+        return installation.html_url
+    }
+}
+
+struct InstallationRepositoriesEvent: Decodable, Notificatable {
+    let action: String
+    let installation: Installation
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) \(action) a GitHub app \(installation.app_id)"
+    }
+    var body: String? {
+        return "That’s all we got" //FIXME
+    }
+    var url: URL? {
+        return installation.html_url
+    }
+}
+
+struct IssueCommentEvent: Decodable, Notificatable {
+    let action: String
+    let issue: Issue
+    let comment: Comment
+    let repository: Repository
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) \(action) a comment"
+    }
+    var body: String? {
+        return "\(repository.full_name)#\(issue.number)"
+    }
+    var url: URL? {
+        return issue.url
+    }
+}
+
+struct IssuesEvent: Decodable, Notificatable {
+    let action: String
+    let issue: Issue
+    let repository: Repository
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) \(action) #\(issue.number)"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return issue.url
+    }
+}
+
+struct LabelEvent: Decodable, Notificatable {
+    let action: String
+    let label: Label
+    let repository: Repository
+    let sender: User
+
+    struct Label: Decodable {
+        let name: String
+        let url: URL
+        let color: String
+    }
+
+    var title: String? {
+        return "\(sender.login) \(action) a label"
+    }
+    var body: String? {
+        return "\(label.name) (\(label.color))"
+    }
+    var url: URL? {
+        return label.url
+    }
+}
+
+struct MemberEvent: Decodable, Notificatable {
+    let action: String
+    let member: User
+    //let changes: Changes
+    let repository: Repository
+    let sender: User
+
+//    struct Changes: Decodable {
+//        let permission: Permission
+//        struct Permission: Decodable {
+//            let from: String?
+//        }
+//    }
+
+    var title: String? {
+        return "\(sender.login) \(action) membership for \(member.login)"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return repository.contributors_url
+    }
+}
+
+struct MembershipEvent: Decodable, Notificatable {
+    let action: String
+    let scope: String
+    let user: User
+    let sender: User
+    let organization: User
+    let team: Team
+
+    struct Team: Decodable {
+        let name: String
+        let url: URL
+    }
+
+    var title: String? {
+        return "\(sender.login) \(action) membership for \(organization.login)"
+    }
+    var body: String? {
+        return "Added to team: \(team.name)"
+    }
+    var url: URL? {
+        return team.url
+    }
+}
+
+struct MilestoneEvent: Decodable, Notificatable {
+    let action: String
+    let sender: User
+    let repository: Repository
+    let milestone: Milestone
+
+    struct Milestone: Decodable {
+        let html_url: URL
+        let title: String
+        let description: String?
+    }
+
+    var title: String? {
+        return "\(sender.login) \(action) a milestone: \(milestone.title)"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return milestone.html_url
+    }
+}
+
+struct OrganizationEvent: Decodable, Notificatable {  //TODO half-arsed
+    let action: String
+    let organization: User
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) \(action) a member"
+    }
+    var body: String? {
+        return "To: " + organization.login
+    }
+}
+
+struct OrgBlockEvent: Decodable, Notificatable {  //TODO half-arsed
+    let action: String
+    let blocked_user: User
+    let organization: User
+    let sender: User
+
+    var title: String? {
+        return "\(sender.login) \(action) \(blocked_user.login)"
+    }
+    var body: String? {
+        return "Org: " + organization.login
+    }
+}
+
+struct PageBuildEvent: Decodable, Notificatable {
+    let build: Build
+    let repository: Repository
+    let sender: User
+
+    struct Build: Decodable {
+        let url: URL
+        let status: String
+        let error: Error?
+
+        struct Error: Decodable {
+            let message: String?
+        }
+    }
+
+    var title: String? {
+        return "GitHub Pages build complete: \(build.status)"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return build.url
+    }
+}
+
+struct PushEvent: Decodable, Notificatable {
+    let repository: Repository
+    let pusher: Pusher
+    let commits: [Commit]
+    let compare: URL
+    let forced: Bool
+    let head_commit: Commit
+
+    struct Pusher: Decodable {
+        let name: String
+    }
+    struct Commit: Decodable {
+        let message: String
+        let url: URL
+    }
+
+    var title: String? {
+        let force = forced ? "force‑" : ""
+        return "\(pusher.name) \(force)pushed to \(repository.full_name)"
+    }
+    var body: String? {
+        if commits.count == 1 {
+            return "Contains 1 commit"
+        } else {
+            return "Contains \(commits.count) commits"
+        }
+    }
+    var url: URL? {
+        return head_commit.url
+    }
+}
+
+struct WatchEvent: Decodable, Notificatable {
+    let action: String
+    let sender: User
+    let repository: Repository
+
+    var title: String? {
+        return "\(sender.login) \(action) \(repository.full_name)"
+    }
+    var body: String? {
+        return repository.full_name
+    }
+    var url: URL? {
+        return repository.html_url
+    }
+}
+
+// types
+
+struct User: Decodable {
     let login: String
 }
+
 struct Repository: Decodable {
     let full_name: String
     let `private`: Bool
+    let html_url: URL
+    let contributors_url: URL
+}
+
+struct Deployment: Decodable {
+    let url: URL
+    let description: String?
+    let environment: String
+}
+
+struct Installation: Decodable {
+    let html_url: URL
+    let app_id: Int
+}
+
+struct Issue: Decodable {
+    let url: URL
+    let number: Int
+}
+
+struct Comment: Decodable {
+    let url: URL
+    let body: String
+    let user: User
 }
--- Sources.Server/routes.swift
+++ Sources.Server/routes.swift
@@ -68,10 +68,61 @@ private func githubHandler(request: HTTPRequest, _ response: HTTPResponse) {
         let data = Data(bytes: bytes)
         String(data: data, encoding: .utf8).map{ print($0) }
 
+        func decode<T>(_ t: T.Type) throws -> T where T: Decodable {
+            return try JSONDecoder().decode(T.self, from: data)
+        }
+
         let notificatable: Notificatable
         switch eventType {
         case "ping":
-            notificatable = try JSONDecoder().decode(PingEvent.self, from: data)
+            notificatable = try decode(PingEvent.self)
+        case "push":
+            notificatable = try decode(PushEvent.self)
+        case "check_run":
+            notificatable = try decode(CheckRunEvent.self)
+        case "check_suite":
+            notificatable = try decode(CheckSuiteEvent.self)
+        case "commit_comment":
+            notificatable = try decode(CommitComment.self)
+        case "create":
+            notificatable = try decode(CreateEvent.self)
+        case "delete":
+            notificatable = try decode(DeleteEvent.self)
+        case "deployment":
+            notificatable = try decode(DeploymentEvent.self)
+        case "deployment_status":
+            notificatable = try decode(DeploymentStatusEvent.self)
+        case "fork":
+            notificatable = try decode(ForkEvent.self)
+        case "gollum":
+            notificatable = try decode(GollumEvent.self)
+        case "installation":
+            notificatable = try decode(InstallationEvent.self)
+        case "installation_repositories":
+            notificatable = try decode(InstallationRepositoriesEvent.self)
+        case "issue_comment":
+            notificatable = try decode(IssueCommentEvent.self)
+        case "issues":
+            notificatable = try decode(IssuesEvent.self)
+        case "label":
+            notificatable = try decode(LabelEvent.self)
+        case "member":
+            notificatable = try decode(MemberEvent.self)
+        case "membership":
+            notificatable = try decode(MembershipEvent.self)
+        case "milestone":
+            notificatable = try decode(MilestoneEvent.self)
+        case "organization":
+            notificatable = try decode(OrganizationEvent.self)
+        case "org_block":
+            notificatable = try decode(OrgBlockEvent.self)
+        case "page_build":
+            notificatable = try decode(PageBuildEvent.self)
+        case "watch":
+            notificatable = try decode(WatchEvent.self)
+        case "marketplace_purchase", "project_card", "project_column", "project", "public", "pull_request", "pull_request_review", "pull_request_review_comment", "release", "repository", "repository_vulnerability_alert", "status", "team", "team_add":
+            print("Unimplemented event:", eventType)
+            fallthrough
         default:
             struct Oh: Notificatable {
                 let title: String?
@@ -80,10 +131,13 @@ private func githubHandler(request: HTTPRequest, _ response: HTTPResponse) {
             notificatable = Oh(title: eventType)
         }
 
-        let notificationItems: [APNSNotificationItem] = [
+        var notificationItems: [APNSNotificationItem] = [
             .alertTitle(notificatable.title ?? "Error"),
             .alertBody(notificatable.body ?? "Error"),
         ]
+        if let url = notificatable.url {
+            notificationItems.append(.customPayload("url", url))
+        }
 
         NotificationPusher(apnsTopic: apnsTopicId).pushAPNS(configurationName: apnsTopicId, deviceTokens: [token], notificationItems: notificationItems) { responses in
             print("APNs said:", responses)
