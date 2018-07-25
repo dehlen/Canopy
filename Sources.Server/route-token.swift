b9977ff86a80401ac8cd209f4e51f078cd9fcb69
Max Howell <mxcl@me.com>
2018-07-25 17:28:36 -0400

    Trying to add the same token twice is ok

--- Sources.Server/route-token.swift
+++ Sources.Server/route-token.swift
@@ -1,3 +1,4 @@
+import enum PerfectSQLite.SQLiteError
 import PerfectHTTP
 import Foundation
 import PromiseKit
@@ -20,5 +21,10 @@ func updateTokens(oauth: String, device: String, apnsTopicId: String) -> Promise
         GitHubAPI(oauthToken: oauth).me()
     }.done { me in
         try DB().add(token: device, topic: apnsTopicId, userId: me.id)
+    }.recover { error in
+        // code 19 means UNIQUE violation, so we already have this, which is fine
+        guard case PerfectSQLite.SQLiteError.Error(let code, _) = error, code == 19 else {
+            throw error
+        }
     }
 }
