2a23b9362487d0927f7e16a21cd3f4ee2cbf88bb
Max Howell <mxcl@me.com>
2018-08-01 01:04:46 -0400

    TODO updates

--- Sources.Client.macOS/App.swift
+++ Sources.Client.macOS/App.swift
@@ -4,19 +4,18 @@ import AppKit
 //TODO store oauth token in at least the keychain
 //TODO encrypt the state parameter or someone will notice and be weird about it
 //TODO prettier authorizing… webpage
-//TODO should use postgres or another not-in-process database since that puts the onus on *us* to never crash
+//TODO look into how sqlite handles crashes and CTRL-C
 //TODO Laurie couldn't see that the webhook was installed, may be necessary to store this info serverside
 //  unless switching to a github app fixes this
 //TODO Add secrets to hooks
 //TODO icon
 //TODO IAP-subscription for private repos
-//TODO ensure private repos only go to valid users (HEAD request with oauth token)
 //TODO FAQ on website that desribes how we get private data briefly (maybe github apps can fix that?)
 //  talks about future server app distibution, talks about how we plan to improve this
 //TODO ensure we are transactional where it counts (let's never break ffs!)
 //TODO db backups
 //TODO organization events have to be directed someplace, maybe API request to get list of members?
-
+//TODO allow disabling actions made by yourself (leave *on* since it is a good wow-moment to get the push when you do stuff)
 
 @NSApplicationMain
 class AppDelegate: NSObject {
