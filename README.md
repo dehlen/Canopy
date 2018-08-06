ae27de273ca10c97caeac0e11addbd2513d9f0df
Max Howell <mxcl@me.com>
2018-08-06 16:28:11 -0400

    Move TODO and FAQ to GitHub

--- README.md
+++ README.md
@@ -11,25 +11,3 @@
     rsync --archive --human-readable --compress --verbose --delete \
           --exclude .build --exclude Carthage --exclude AuthKey_5354D789X6.p8 \
           . canopy:src
-  
-# FAQ
-
-> I cannot see a particular private repo.
-
-The GitHub API does not return results for private repositories forked from private repositories that are not organizations.
-To add such repositories you have to create a personal-access-token and replace the keychain entry for Canopy with that token.
-
-> I do not get certain notifications I was expecting
-
-Canopy can only report activity that is supported by GitHub’s webhook system, notably this does not include:
-
-* Activity on other people’s repositories that you are watching
-* Activity on issue trackers that occurs after you have commented
-* Activity on your gists
-* Repository rename events
-
-> Why do you ask for full access to my repositories
-
-Unfortunately this permission is the lowest we can request in order to list your private repositories.
-
-GitHub need to offer more granularity in their permission scope system.
--- Sources.Client.macOS/App.swift
+++ Sources.Client.macOS/App.swift
@@ -1,36 +1,6 @@
 import PromiseKit
 import AppKit
 
-//TODO the infinite notifications bug is quite a problem
-//   either debug it or just switch to Vapor
-
-//TODO icon
-//TODO need a safe way to shutdown the server app
-//TODO can we rely on push to deliver the oauth token back? Debug mode suggests not.
-//TODO research scaling AWS instance
-//TODO prettier authorizing… webpage
-//TODO FAQ on website that desribes how we get private data briefly (maybe github apps can fix that?)
-//  talks about future server app distibution, talks about how we plan to improve this
-//TODO validate and verify hook secrets
-
-//TODO organization events have to be directed someplace, maybe API request to get list of members?
-//TODO ensure we keep SSL certificate up-to-date, look into automating it
-//TODO ensure our Apple Developer account renews 
-//TODO look into how sqlite handles crashes and CTRL-C
-//TODO db backups
-//TODO ensure we are transactional where it counts (let's never break ffs!)
-//TODO allow disabling actions made by yourself (leave *on* since it is a good wow-moment to get the push when you do stuff)
-//TODO organization subscription plan
-//TODO uninstall webhook
-//TODO get a read-only token for server: https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/#creating-multiple-tokens-for-oauth-apps
-//TODO check for webhook on github end if user is admin when selecting repo or organization in the outline-view
-
-//TODO actually it is possible to create multiple hooks with the same configuration
-//  well, maybe if the secret varies? Maybe should make the secret some one way hash of a salt + the repo name instead?
-//  eitherway should detect this and prevent it.
-//TODO restore checking that the webhook is still installed somewhere, since user can delete it themselves (or switch to app!)
-//TODO show red if notify enabled by webhook has been removed (can do periodic test server side using test API)
-
 @NSApplicationMain
 class AppDelegate: NSObject {
     weak var window: NSWindow!
