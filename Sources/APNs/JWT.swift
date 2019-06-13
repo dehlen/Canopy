0b65d961609d54a8cf575e14372a6c1bce43ca0e
Max Howell <mxcl@me.com>
2019-06-13 17:32:16 -0400

    Attempt to prevent too many token updates

--- Sources/APNs/JWT.swift
+++ Sources/APNs/JWT.swift
@@ -6,22 +6,29 @@ private let teamId = "TEQMQBRC7B"
 
 private let q = DispatchQueue(label: #file, attributes: .concurrent)
 private var sigtime = Date(timeIntervalSince1970: 0)
-private var lastJwt: String!
+private var lastJwt = ""
 
 var jwt: String {
-    //TODO thread-safety
     let now = Date()
-    let t = q.sync{ sigtime }
-    if t.timeIntervalSince(now) < -3590 {
-        return q.sync(flags: .barrier) {
-            let payload: [String: Any] = ["iss": teamId, "iat": Int(now.timeIntervalSince1970)]
-            let jwt = JWTCreator(payload: payload)!
-            let pem = try! PEMKey(pemPath: "../AuthKey_5354D789X6.p8")
-            lastJwt = try! jwt.sign(alg: JWT.Alg.es256, key: pem, headers: ["kid": "5354D789X6"])
-            sigtime = now
+    let jwt = q.sync{ (time: sigtime, token: lastJwt) }
+
+    if now.timeIntervalSince(jwt.time) >= 3590 {
+        return jwt.token
+    }
+
+    // generate new token
+    return q.sync(flags: .barrier) {
+
+        // yay for async!
+        guard now.timeIntervalSince(sigtime) >= 3590 else {
             return lastJwt
         }
-    } else {
-        return q.sync{ lastJwt }
+
+        let payload: [String: Any] = ["iss": teamId, "iat": Int(now.timeIntervalSince1970)]
+        let jwt = JWTCreator(payload: payload)!
+        let pem = try! PEMKey(pemPath: "../AuthKey_5354D789X6.p8")
+        lastJwt = try! jwt.sign(alg: JWT.Alg.es256, key: pem, headers: ["kid": "5354D789X6"])
+        sigtime = now // slightly off, but ok
+        return lastJwt
     }
 }
